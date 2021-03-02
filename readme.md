9f98123a0469340916b0b6a379272ab454ef4edd
Artem Yurchenko <artemyurchenko@zoho.com>
2021-03-03 00:23:17 +0300

    add initial project structure;
        add a contributing guide;
    
        add a basic gradient descent implementation;

---
+++ readme.md
@@ -0,0 +1,3 @@
+# methopt — we know how to optimize!
+
+If you are a developer, please refer to [contributing.md](contributing.md)
---
+++ tests/grad_descent_test.py
@@ -0,0 +1,15 @@
+from methopt.grad_descent import grad_descent
+
+EPS = 1e-7
+
+
+def approx_equal(a, b):
+    return abs(a - b) < EPS
+
+
+def test_grad_descent_parabole():
+    f = lambda x: (x - 3) ** 2 + 8
+    f_grad = lambda x: 2 * (x - 3)
+    x0 = -6
+
+    assert approx_equal(grad_descent(f, f_grad, x0), 3)
