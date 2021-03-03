6764bd3f85e4950e476b3a2d5b9e02ab55188a2e
Artem Yurchenko <artemyurchenko@zoho.com>
2021-03-03 15:37:10 +0300

    support one-dimensional methods in a gradient descent

--- src/methopt/grad_descent.py
+++ src/methopt/grad_descent.py
@@ -1,30 +1,4 @@
-EPS = 1e-7
-
-
-def divide_step_strategy(f, x, step_prev, iteration_no, mk_new_x_callback):
-    """A strategy to find a new gradient step.
-
-    This works by taking the old step and dividing it by two, until a
-    function value in the next point is not less than the value in the
-    current point.
-
-    Once in 100 iterations we try to make a gradient step much bigger,
-    so as too speed up a search.
-
-    """
-    if iteration_no % 100 == 0:  # try bigger step
-        step = step_prev * 256
-    else:
-        step = step_prev
-
-    x_new = mk_new_x_callback(step)
-    fx = f(x)
-    # HMM perhaps need to make floating-point-tailored comparison
-    while f(x_new) >= fx and step > EPS:
-        step /= 2
-        x_new = mk_new_x_callback(step)
-
-    return step
+import methopt.step_adjustment_strategy as strat
 
 
 def grad_descent(
@@ -34,6 +8,7 @@ def grad_descent(
     max_iterations_count=1000,
     step_adjustment_strategy="divide_step",
     initial_step=1,
+    eps=None,
 ):
     """Find an approximation of a local minimum of the function.
 
@@ -47,36 +22,35 @@ def grad_descent(
     'max_iterations_count' — a maximum number of iterations to do
     before stopping at the best guess.
 
-    'step_adjustment_strategy' — a name of a strategy that adjusts a
-    gradient step on every iteration. Available strategies:
-    '\"grad_descent.divide_step_strategy\"'
+    'step_adjustment_strategy' — a name of a strategy or an object of
+    strategy that adjusts a gradient step on every
+    iteration. Available strategies can be found in
+    ./step_adjustment_strategy.py
 
     'intial_step' — an initial gradient step.
 
+    'eps' — a margin for floating-point comparisons, default is 1e-7
+
     """
 
+    if eps is None:
+        eps = 1e-7
+
     if step_adjustment_strategy == "divide_step":
-        step_adjustment_strategy = divide_step_strategy
-    else:
+        step_adjustment_strategy = strat.DivideStepStrategy(f, f_grad, eps=eps)
+    elif not isinstance(step_adjustment_strategy, strat.StepAdjustmentStrategy):
         raise GradDescentException(
             f"Unknown step adjustment strategy: {step_adjustment_strategy}"
         )
 
     x = x0
     step_prev = initial_step
-    mk_new_x_callback = lambda step: x - step * f_grad(x)
     for iteration_no in range(max_iterations_count):
-        step = step_adjustment_strategy(
-            f,
-            x,
-            step_prev=step_prev,
-            iteration_no=iteration_no,
-            mk_new_x_callback=mk_new_x_callback,
-        )
-        x_new = mk_new_x_callback(step)
+        step = step_adjustment_strategy(x, step_prev, iteration_no)
+        x_new = x - step * f_grad(x)
 
         # TODO add a function parameter to choose a stopping criterion
-        if abs(x - x_new) < EPS:
+        if abs(x - x_new) < eps:
             break
 
         step_prev = step
--- tests/grad_descent_test.py
+++ tests/grad_descent_test.py
@@ -1,15 +1,55 @@
 from methopt.grad_descent import grad_descent
+import methopt.step_adjustment_strategy as step
 
 EPS = 1e-7
 
 
-def approx_equal(a, b):
-    return abs(a - b) < EPS
+def approx_equal(a, b, eps=EPS):
+    return abs(a - b) < eps
 
 
-def test_grad_descent_parabole():
+def test_grad_descent_divide_step():
     f = lambda x: (x - 3) ** 2 + 8
     f_grad = lambda x: 2 * (x - 3)
     x0 = -6
 
-    assert approx_equal(grad_descent(f, f_grad, x0), 3)
+    assert approx_equal(
+        grad_descent(f, f_grad, x0, step_adjustment_strategy="divide_step"), 3
+    )
+
+
+def test_grad_descent_dichotomy():
+    f = lambda x: (x - 3) ** 2 + 8
+    f_grad = lambda x: 2 * (x - 3)
+    x0 = -6
+
+    strategy = step.DichotomyStrategy(f, f_grad, max_step=1000, max_steps=20, eps=1e-8)
+    assert approx_equal(
+        grad_descent(f, f_grad, x0, step_adjustment_strategy=strategy),
+        3,
+        eps=1e-4,  # it does not perform well -_-. so we set a lower correctness margin
+    )
+
+
+def test_grad_descent_golden_section():
+    f = lambda x: (x - 3) ** 2 + 8
+    f_grad = lambda x: 2 * (x - 3)
+    x0 = -6
+
+    strategy = step.GoldenSectionStrategy(
+        f, f_grad, max_step=1000, max_steps=20, eps=1e-8
+    )
+    assert approx_equal(
+        grad_descent(f, f_grad, x0, step_adjustment_strategy=strategy), 3
+    )
+
+
+def test_grad_descent_fibonacci():
+    f = lambda x: (x - 3) ** 2 + 8
+    f_grad = lambda x: 2 * (x - 3)
+    x0 = -6
+
+    strategy = step.FibonacciStrategy(f, f_grad, max_step=1000, max_steps=20, eps=1e-8)
+    assert approx_equal(
+        grad_descent(f, f_grad, x0, step_adjustment_strategy=strategy), 3
+    )
