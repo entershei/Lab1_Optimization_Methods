614d66bd0bd109a132c38e4c9e84fbf4382b4c25
imd <olegfafurin1@gmail.com>
2021-03-05 23:21:57 +0300

    add trajectories for different step adjustment strategies

--- src/methopt/graphic/trajectory.py
+++ src/methopt/graphic/trajectory.py
@@ -1,4 +1,5 @@
 from methopt.grad_descent import grad_descent
+import methopt.step_adjustment_strategy as strat
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -42,15 +43,79 @@ def draw_2d(f, f_grad, x0, title, step_adjustment_strategy=None):
 def main():
     f0 = lambda x: 2 * x[0] ** 2 + x[1] ** 2
     f0_grad = lambda x: np.array([4 * x[0], 2 * x[1]])
-    draw_2d(f0, f0_grad, np.array([1, 10]), f"2x^2+y^2+5_(1;10)")
+    draw_2d(f0, f0_grad, np.array([1, 10]), title="2x^2+y^2+5_(1;10)")
 
     f1 = lambda x: x[0] ** 2 + 10 * x[1] ** 2 + 5
     f1_grad = lambda x: np.array([2 * x[0], 20 * x[1]])
-    draw_2d(f1, f1_grad, np.array([5, -7]), f"x^2+10y^2+5_(5;-7)")
+    draw_2d(f1, f1_grad, np.array([5, -7]), title="x^2+10y^2+5_(5;-7)")
 
-    f3 = lambda x: (x[0] - 3) ** 2 + (x[1] - 1) ** 2 + 1
-    f3_grad = lambda x: np.array([2 * x[0] - 6, 2 * x[1] - 2])
-    draw_2d(f3, f3_grad, np.array([-3, 3]), title="(x-3)^2+(y-1)^2+1_(-3;3)")
+    f2 = lambda x: (x[0] - 3) ** 2 + (x[1] - 1) ** 2 + 1
+    f2_grad = lambda x: np.array([2 * x[0] - 6, 2 * x[1] - 2])
+    draw_2d(f2, f2_grad, np.array([-3, 3]),
+            title="(x-3)^2+(y-1)^2+1_(-3;3)")
+
+    f0_fib = lambda x: 2 * x[0] ** 2 + x[1] ** 2
+    f0_fib_grad = lambda x: np.array([4 * x[0], 2 * x[1]])
+    draw_2d(f0_fib, f0_fib_grad, np.array([1, 10]),
+            title="2x^2+y^2+5_(1;10)_fib",
+            step_adjustment_strategy=strat.FibonacciStrategy(f0_fib, f0_fib_grad, max_step=1000, max_steps=20,
+                                                             eps=1e-8))
+
+    f1_fib = lambda x: x[0] ** 2 + 10 * x[1] ** 2 + 5
+    f1_fib_grad = lambda x: np.array([2 * x[0], 20 * x[1]])
+    draw_2d(f1_fib, f1_fib_grad, np.array([5, -7]),
+            title="x^2+10y^2+5_(5;-7)_fib",
+            step_adjustment_strategy=strat.FibonacciStrategy(f1_fib, f1_fib_grad, max_step=1000, max_steps=20,
+                                                             eps=1e-8))
+
+    f2_fib = lambda x: (x[0] - 3) ** 2 + (x[1] - 1) ** 2 + 1
+    f2_fib_grad = lambda x: np.array([2 * x[0] - 6, 2 * x[1] - 2])
+    draw_2d(f2_fib, f2_grad, np.array([-3, 3]),
+            title="(x-3)^2+(y-1)^2+1_(-3;3)_fib",
+            step_adjustment_strategy=strat.FibonacciStrategy(f2_fib, f2_fib_grad, max_step=1000, max_steps=20,
+                                                             eps=1e-8))
+
+    f0_dich = lambda x: 2 * x[0] ** 2 + x[1] ** 2
+    f0_dich_grad = lambda x: np.array([4 * x[0], 2 * x[1]])
+    draw_2d(f0_dich, f0_dich_grad, np.array([1, 10]),
+            title="2x^2+y^2+5_(1;10)_dich",
+            step_adjustment_strategy=strat.DichotomyStrategy(f0_dich, f0_dich_grad, max_step=1000, max_steps=20,
+                                                             eps=1e-8))
+
+    f1_dich = lambda x: x[0] ** 2 + 10 * x[1] ** 2 + 5
+    f1_dich_grad = lambda x: np.array([2 * x[0], 20 * x[1]])
+    draw_2d(f1_dich, f1_dich_grad, np.array([5, -7]),
+            title="x^2+10y^2+5_(5;-7)_dich",
+            step_adjustment_strategy=strat.DichotomyStrategy(f1_dich, f1_dich_grad, max_step=1000, max_steps=20,
+                                                             eps=1e-8))
+
+    f2_dich = lambda x: (x[0] - 3) ** 2 + (x[1] - 1) ** 2 + 1
+    f2_dich_grad = lambda x: np.array([2 * x[0] - 6, 2 * x[1] - 2])
+    draw_2d(f2_dich, f2_dich_grad, np.array([-3, 3]),
+            title="(x-3)^2+(y-1)^2+1_(-3;3)_dich",
+            step_adjustment_strategy=strat.DichotomyStrategy(f2_dich, f2_dich_grad, max_step=1000, max_steps=20,
+                                                             eps=1e-8))
+
+    f0_gold = lambda x: 2 * x[0] ** 2 + x[1] ** 2
+    f0_gold_grad = lambda x: np.array([4 * x[0], 2 * x[1]])
+    draw_2d(f0_gold, f0_gold_grad, np.array([1, 10]), title="2x^2+y^2+5_(1;10)_gold",
+            step_adjustment_strategy=strat.GoldenSectionStrategy(
+                f0_gold, f0_gold_grad, max_step=1000, max_steps=20, eps=1e-8
+            ))
+
+    f1_gold = lambda x: x[0] ** 2 + 10 * x[1] ** 2 + 5
+    f1_gold_grad = lambda x: np.array([2 * x[0], 20 * x[1]])
+    draw_2d(f1_gold, f1_gold_grad, np.array([5, -7]), title="x^2+10y^2+5_(5;-7)_gold",
+            step_adjustment_strategy=strat.GoldenSectionStrategy(
+                f1_gold, f1_gold_grad, max_step=1000, max_steps=20, eps=1e-8
+            ))
+
+    f2_gold = lambda x: (x[0] - 3) ** 2 + (x[1] - 1) ** 2 + 1
+    f2_gold_grad = lambda x: np.array([2 * x[0] - 6, 2 * x[1] - 2])
+    draw_2d(f2_gold, f2_gold_grad, np.array([-3, 3]), title="(x-3)^2+(y-1)^2+1_(-3;3)_gold",
+            step_adjustment_strategy=strat.GoldenSectionStrategy(
+                f2_gold, f2_gold_grad, max_step=1000, max_steps=20, eps=1e-8
+            ))
 
 
 if __name__ == "__main__":
